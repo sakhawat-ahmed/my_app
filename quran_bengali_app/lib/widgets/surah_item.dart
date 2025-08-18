8c4bab11611b04fbee3f2586300cc08034ffdc9b
sakhawat6927 <rakiber357@gmail.com>
2025-08-18 23:00:01 +0800

    create services and widgets

---
+++ quran_bengali_app/lib/widgets/surah_item.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:quran_bengali_app/models/surah_model.dart';
+
+class SurahItem extends StatelessWidget {
+  final Surah surah;
+  final VoidCallback onTap;
+
+  const SurahItem({
+    Key? key,
+    required this.surah,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Row(
+          children: [
+            Container(
+              width: 40,
+              height: 40,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Text(
+                surah.number.toString(),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    surah.englishName,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    surah.englishNameTranslation,
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              surah.name,
+              style: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'QuranFont',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
