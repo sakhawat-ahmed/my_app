999d8ac1265993c32cf99cabc54e98fd9f0a3e56
sakhawat6927 <rakiber357@gmail.com>
2025-10-09 11:03:13 +0800

    update settings screen for colorful ui

--- grocery_app/lib/widgets/setting_tile.dart
+++ grocery_app/lib/widgets/setting_tile.dart
@@ -6,6 +6,7 @@ class SettingTile extends StatelessWidget {
   final String subtitle;
   final VoidCallback onTap;
   final bool isDestructive;
+  final List<Color>? gradient;
 
   const SettingTile({
     super.key,
@@ -14,30 +15,90 @@ class SettingTile extends StatelessWidget {
     required this.subtitle,
     required this.onTap,
     this.isDestructive = false,
+    this.gradient,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(
-        icon,
-        color: isDestructive ? Colors.red : Colors.green,
-      ),
-      title: Text(
-        title,
-        style: TextStyle(
-          color: isDestructive ? Colors.red : Colors.black,
-          fontWeight: FontWeight.w500,
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          decoration: gradient != null
+              ? BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: gradient!,
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                )
+              : null,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Row(
+              children: [
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: gradient != null
+                        ? Colors.white.withOpacity(0.2)
+                        : (isDestructive
+                            ? Colors.red.withOpacity(0.1)
+                            : Colors.green.withOpacity(0.1)),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Icon(
+                    icon,
+                    color: gradient != null
+                        ? Colors.white
+                        : (isDestructive ? Colors.red : Colors.green),
+                    size: 20,
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        style: TextStyle(
+                          color: gradient != null
+                              ? Colors.white
+                              : (isDestructive ? Colors.red : Colors.black),
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        subtitle,
+                        style: TextStyle(
+                          color: gradient != null
+                              ? Colors.white.withOpacity(0.8)
+                              : (isDestructive
+                                  ? Colors.red.withOpacity(0.7)
+                                  : Colors.grey[600]),
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 16,
+                  color: gradient != null
+                      ? Colors.white
+                      : (isDestructive ? Colors.red : Colors.grey),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
-      subtitle: Text(
-        subtitle,
-        style: TextStyle(
-          color: isDestructive ? Colors.red.withOpacity(0.7) : Colors.grey[600],
-        ),
-      ),
-      trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-      onTap: onTap,
     );
   }
 }
